c874b7572f2788063d629226b08eba888477edc9
Sina <sinaa1383@gmail.com>
2024-05-27 15:47:23 +0330

    commit origin

---
+++ app/src/main/java/xyz/sina/dowr/tutorial/Tutorial.kt
@@ -0,0 +1,14 @@
+package xyz.sina.dowr.tutorial
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import xyz.sina.dowr.utils.Texts
+
+
+@Composable
+fun Tutorial(){
+
+    Text(text = Texts.tutorialFirst)
+
+
+}
